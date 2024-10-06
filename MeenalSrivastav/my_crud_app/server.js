1f0e35c871139c216d56ca47e5555837ffaba85b
MeenalSrivas <meenalsrivastav2004@gmail.com>
2024-10-06 17:51:08 +0530

    meenal's fourth commit

--- MeenalSrivastav/my_crud_app/server.js
+++ MeenalSrivastav/my_crud_app/server.js
@@ -2,11 +2,16 @@ const express =  require('express')
 const app = express()
 const dotenv = require("dotenv")
 var bodyParser = require("body-parser")
-dotenv.config();
 const connection = require("./config/db")
-app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended: false }));
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
 app.use(express.json());
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
+
+
 
 
 
@@ -15,28 +20,42 @@ app.get("/", (req, res) => {
     res.redirect("/create.html");
   });
 
-  app.post("/create",(req,res) =>{
-    const name = req.body.name;
-    const email = req.body.email;
-
-    try{
-        connection.query(
-            "insert into student_info (name, email) values(?,?)",
-            [name, email],
-            (err, rows) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.send(rows);
-                }
-            }
-        );
+//read operation which will be passing value to ejs engine
+app.get("/data", (req, res) => {
+    const allData = "select * from student_info";
+    connection.query(allData, (err, rows) => {
+      if (err) {
+        res.send(err);
+      } else {
+        // res.json({ rows });
+        res.render("read.ejs", { rows });  // redering read.ejs file along with data
+      }
+    });
+  });
+//create
+app.post("/create", (req, res) => {   
+    console.log(req.body.name);
+    var name = req.body.name;
+    var email = req.body.email;
+    try {
+      connection.query(
+        "INSERT into demo123 (name,email) values(?,?)",  //2. saving in database
+        [name, email],
+        function (err, result) {
+          if (err) {
+            console.log(err);
+          } else {
+            // res.json({ result });
+            res.redirect("/data");  //3. Redirect user to the new page
+          }
+        }
+      );
     } catch (err) {
-        console.log(err);
+      res.send(err);
     }
   });
-  
-app.listen(process.env.PORT || 4000, (error) => {
+
+app.listen(process.env.PORT || 4000, function(error) {
     if (error) throw error;
     console.log(`server running on port ${process.env.PORT}`);
 });
