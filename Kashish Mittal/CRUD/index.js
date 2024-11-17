48b1c81815c4d5d669e374ae36c022b39a60896d
kashishmi <kashishmittal686@gmail.com>
2024-11-17 22:51:50 +0530

    Some modifications

---
+++ Kashish Mittal/CRUD/index.js
@@ -0,0 +1,19 @@
+const express =require("express")
+const app =express();
+const user = require("./database");
+
+app.set("view engine", "ejs")
+app.get("/",(req,res)=>{
+    res.render("index",{
+        title:"this is index page",
+        text:"this is home page",
+
+    })
+
+})
+
+
+
+app.listen(5000,()=>{
+    console.log("server listening on port :5000")
+})
