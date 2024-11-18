ae41e6c6c0bf7b91c8bc140b87ad497a65a2bb3b
kashishmi <kashishmittal686@gmail.com>
2024-11-18 19:33:59 +0530

    Portfolio changes

--- Kashish Mittal/CRUD/database.js
+++ Kashish Mittal/CRUD/database.js
@@ -1,15 +1,11 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/crudapp").then(()=>{
-  console.log("connection successful")  
-}).catch((e)=>{
-    console.log(e);
+const mysql = require('mysql2/promise')
+
+const mysqlPool = mysql.createPool({
+    host: 'localhost',
+    user: 'Kashish',
+    password: 'kashish1234',
+    database: 'employee_db'
 })
 
-const schema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-})
 
-const Usermodel = mongoose.model("User",Schema);
-module.exports = Usermodel;
+module.exports = mysqlPool
