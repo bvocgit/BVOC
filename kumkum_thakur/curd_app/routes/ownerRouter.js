ef284fcb013f52a046c7a3e81548c41162fdc338
kumkum <kumkumthakur2411@gmail.com>
2024-11-21 11:30:34 -0800

    ownerModel setup and owner is created

--- kumkum_thakur/curd_app/routes/ownerRouter.js
+++ kumkum_thakur/curd_app/routes/ownerRouter.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router =express.Router();
+const ownerModel = require("../models/owners-model");
+if(process.env.NODE_ENV === "development"){
+    router.post("/create",async function(req,res){
+      let owners = await  ownerModel.find();
+      if(owners.length > 0){
+        return res
+        .status(503)
+        .send("you do not have permissson to create a new owner");
+    } 
+    let {fullname, email,password} = req.body;
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password 
+    })
+    res.status(201).send(createdOwner);
+    });
+}
 
 router.get("/", function(req,res){
     res.send("hey");
 });
 
+//  console.log(process.env.NODE_ENV);
 module.exports =router;
